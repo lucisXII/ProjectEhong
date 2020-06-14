abd84fe69cbe51ce4d1a069663b95e8ee8e33be3
unknown <tears.of.devils.black@gmail.com>
2020-06-15 00:42:44 +0700

    new

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'http://localhost:8000/api'
+  //apiUrl: 'http://192.168.1.10:8000/api'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000/api'
+  //apiUrl: 'http://192.168.1.10:8000/api'
 };
 
 /*

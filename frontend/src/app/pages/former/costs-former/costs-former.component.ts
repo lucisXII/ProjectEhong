6476a9311d3be4c13832c53d5658cfea82b90dd2
unknown <tears.of.devils.black@gmail.com>
2020-06-19 13:43:18 +0700

    frontend former

--- frontend/src/app/pages/former/costs-former/costs-former.component.ts
+++ frontend/src/app/pages/former/costs-former/costs-former.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { FormerService } from 'src/app/services/former.service';
 
 @Component({
   selector: 'app-costs-former',
@@ -7,8 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CostsFormerComponent implements OnInit {
 
-  constructor() { }
+  private id: string;
+  private month: string;
+  private year: string;
+  total: any;
+  constructor(
+    private route: ActivatedRoute,
+    private FormerService: FormerService
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.route.paramMap.subscribe((paraMap: ParamMap) => {
+      if(paraMap.has('id')){
+        this.id = paraMap.get('id');
+        this.year = paraMap.get('year');
+        this.month = paraMap.get('month');
+        console.log(this.id);
+
+        this.FormerService.getCostOld(this.id,this.month,this.year)
+        .subscribe(response => {
+          this.total = response;
+          console.log(this.total);
+        });
+
+      }
+    });
+  }
 
 }
--- frontend/src/app/pages/former/tools-former/tools-former.component.ts
+++ frontend/src/app/pages/former/tools-former/tools-former.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormerService } from 'src/app/services/former.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-tools-former',
@@ -7,8 +9,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolsFormerComponent implements OnInit {
 
-  constructor() { }
+  private id: string;
+  private month: string;
+  private year: string;
+  tools: any;
+  amountRemain: any;
+  amountChecked: any;
+  numberRemain: any;
+  numberChecked: any;
 
-  ngOnInit() {}
+  constructor(
+    private route: ActivatedRoute,
+    private FormerService: FormerService
+  ) { }
 
+  ngOnInit() {
+    this.route.paramMap.subscribe((paraMap: ParamMap) => {
+      if(paraMap.has('id')){
+        this.id = paraMap.get('id');
+        this.year = paraMap.get('year');
+        this.month = paraMap.get('month');
+        console.log(this.id);
+
+        this.FormerService.getToolsCheckedOld(this.id,this.month,this.year)
+        .subscribe(response => {
+          this.tools = response;
+          console.log(this.tools);
+        });
+
+        this.FormerService.getAmountRemainOldTool(this.id,this.month,this.year)
+        .subscribe(response => {
+          this.amountRemain = response;
+          console.log(this.amountRemain);
+        });
+        this.FormerService.getAmountCheckedOldTool(this.id,this.month,this.year)
+        .subscribe(response => {
+          this.amountChecked = response;
+          console.log(this.amountChecked);
+        });
+        this.FormerService.getNumberRemainOldTool(this.id,this.month,this.year)
+        .subscribe(response => {
+          this.numberRemain = response;
+          console.log(this.numberRemain);
+        });
+        this.FormerService.getNumberCheckedOldTool(this.id,this.month,this.year)
+        .subscribe(response => {
+          this.numberChecked = response;
+          console.log(this.numberChecked);
+        });
+
+      }
+    });
+  }
 }
